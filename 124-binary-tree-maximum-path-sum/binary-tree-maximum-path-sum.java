a1201c0ec6030595d082d355c062e7631e07a1a5
Sukil Raj.M.S <sukilraj2005@gmail.com>
2026-03-17 11:30:39 +0530

    Time: 1 ms (31.45%) | Memory: 46.5 MB (93.43%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
@@ -1,20 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public int maxPathSum(TreeNode root) {
-        int[] res = { root.val };
-        dfs(root, res);
-        return res[0];
+        int [] maxi = new int[1];
+        maxi[0] = Integer.MIN_VALUE;
+        maxpath(root, maxi);
+        return maxi[0];
     }
+    private int maxpath(TreeNode root, int maxi[]){
+        if(root==null) return 0;
 
-    private int dfs(TreeNode node, int[] res) {
-        if (node == null) {
-            return 0;
-        }
-
-        int leftSum = Math.max(0, dfs(node.left, res));
-        int rightSum = Math.max(0, dfs(node.right, res));
-
-        res[0] = Math.max(res[0], leftSum + rightSum + node.val);
-
-        return Math.max(leftSum, rightSum) + node.val;
-    }    
+        int leftsum = Math.max(0,maxpath(root.left, maxi));
+        int rightsum = Math.max(0,maxpath(root.right, maxi));
+        maxi[0] = Math.max(maxi[0], root.val+leftsum+rightsum);
+        return Math.max(leftsum, rightsum) + root.val;
+    }
 }
