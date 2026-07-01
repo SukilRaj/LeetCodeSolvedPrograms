df4882e792fa022e2af293b7d9b28d4865b37eae
Sukil Raj.M.S <sukilraj2005@gmail.com>
2026-07-01 21:02:35 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (86.27%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n<=3) return n;
+
+        int pre1 = 3;
+        int pre2 = 2;
+        int cur = 0;
+        for(int i=3;i<n;i++){
+            cur = pre1+pre2;
+            pre2 = pre1;
+            pre1 = cur;
+        }
+        return cur;
+    }
+}
